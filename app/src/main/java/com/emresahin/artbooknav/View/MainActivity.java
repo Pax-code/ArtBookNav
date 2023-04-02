4e86cfb36ff26caa318bcecac6e7a5b6ec39e90a
Emre Şahin <sahinemre899@gmail.com>
2023-04-02 19:52:14 +0300

    Experimental fade background added

--- app/src/main/java/com/emresahin/artbooknav/View/MainActivity.java
+++ app/src/main/java/com/emresahin/artbooknav/View/MainActivity.java
@@ -2,10 +2,15 @@ package com.emresahin.artbooknav.View;
 
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,11 +21,14 @@ import com.emresahin.artbooknav.R;
 
 
 public class MainActivity extends AppCompatActivity {
-
+AnimationDrawable animationDrawable;
+ConstraintLayout constraintLayout;
+    ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fadeBackground();
 
     }
 
@@ -39,5 +47,16 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
 
+    }
+    public void fadeBackground(){
+        actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#A3FCA7FF")));
+
+
+        constraintLayout = findViewById(R.id.mainLayout);
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(1500);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
     }
 }
